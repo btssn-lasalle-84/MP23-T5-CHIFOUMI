43381cf2f19038c7c66464b6177e59a112ea7aff
b-gaume <benjamin.gaume.pro@gmail.com>
2023-01-05 14:09:42 +0100

    MP23T5-11 Squelette de la classe IHM

---
+++ IHM.h
@@ -0,0 +1,17 @@
+#ifndef IHM_H
+#define IHM_H
+
+#include <string>
+
+class IHM
+{
+  private:
+  public:
+    IHM();
+    void menuPrincipal();
+    void demarrerPartie();
+    void afficherHistorique();
+    void quitterJeu();
+};
+
+#endif // IHM_H
