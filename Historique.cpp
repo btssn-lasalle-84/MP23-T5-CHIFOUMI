469a28d7eb9c3ee60024acccde5f6245abc8c9a6
nrouanet <rouanet.nicolas.pro@gmail.com>
2023-01-06 11:28:29 +0100

    MP23T5-19 Correction de la classe Historique

---
+++ Historique.cpp
@@ -0,0 +1,14 @@
+#include "Historique.h"
+
+Historique::Historique()
+{
+}
+
+Historique::Historique(unsigned int nbDeParties): nbDeParties(nbDeParties)
+{
+}
+
+unsigned int Historique::getHistorique() const
+{
+    return 0;
+}
